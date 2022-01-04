f6153c944af0e043d47b7fd50b89beed29c0d847
Poulenas <Poulenas@LAPTOP-LHDB2PDG>
2022-01-04 23:25:23 +0100

    Speed click On ne peut pas cliquer 2 fois sur démarrer partie

--- Speed-click_modeGraphique/src/speed/click/Bouton_Graphique.java
+++ Speed-click_modeGraphique/src/speed/click/Bouton_Graphique.java
@@ -15,6 +15,8 @@ import javax.swing.JButton;
 public class Bouton_Graphique extends JButton{
   Bouton BoutonAssociee  ; // Attribut 
   ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/image/noir.jpg"));
+  ImageIcon img_verte = new javax.swing.ImageIcon(getClass().getResource("/image/vert.jpg"));
+  ImageIcon img_rouge = new javax.swing.ImageIcon(getClass().getResource("/image/rouge.jpg"));
   
   public Bouton_Graphique(Bouton UnBouton){ // Constructeur avce paramètre 
       BoutonAssociee = UnBouton; 
@@ -23,6 +25,11 @@ public class Bouton_Graphique extends JButton{
   @Override
   public void paintComponent(Graphics G){ // Méthode déjà existante dans java 
       super.paintComponent(G); 
+      if(BoutonAssociee.EtreAllume(BoutonAssociee)==true ){ // Si présence d'un bouton vert
+          setIcon(img_verte); // Case devient verte
+      } else if (BoutonAssociee.EtreUnBoutonPiege()==true){ // Si présence d'un bouton rouge 
+          setIcon(img_rouge); // Case devient rouge 
+      }
       setIcon(img_vide); // On attribue l'image noir.jpg
   }
     
